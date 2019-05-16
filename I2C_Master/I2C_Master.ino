ac8b6e5dedaea389cab7574c56bf6e6395f5a04b
Aidan <aidangoettsch@gmail.com>
2019-05-16 10:23:43 -0400

    Update I2C_Master.ino

--- I2C_Master/I2C_Master.ino
+++ I2C_Master/I2C_Master.ino
@@ -6,11 +6,12 @@ const int US_REGISTER_TIME = 0x02;
 
 void setup() {
   Wire.begin();
-
+  Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println(readByte(US_I2C_ADDRESS, US_REGISTER_DURATION));
+  readByte(US_I2C_ADDRESS, US_REGISTER_DURATION);
+  delay(1000);
 }
 
 uint8_t readByte(int i2cAddress, uint8_t read_register) {
@@ -23,7 +24,11 @@ uint8_t readByte(int i2cAddress, uint8_t read_register) {
 
   // step 5: receive reading from sensor
   if (1 <= Wire.available()) { // if two bytes were received
-    return Wire.read();
+    Serial.print("hmm ");
+    int avail = Wire.available();
+    Serial.print("avail");
+    Serial.print(" ");
+    uint8_t val = Wire.read();
+    Serial.println(val);
   }
-
 }
