550ff4bcf4f4983543fc56274d6a16c55090ee43
Aidan <aidangoettsch@gmail.com>
2019-05-16 10:24:12 -0400

    Update Ultrasonic_I2C.ino

--- Ultrasonic_I2C/Ultrasonic_I2C.ino
+++ Ultrasonic_I2C/Ultrasonic_I2C.ino
@@ -1,4 +1,3 @@
-#include <SoftwareSerial.h>
 #include <Wire.h>
 
 const int I2C_ADDRESS = 0x40;
@@ -12,25 +11,23 @@ uint8_t opcode;
 uint8_t duration;
 uint8_t readingTime;
 
-SoftwareSerial OpenLog(2, 3);
-
 void setup() {
   Serial.begin(9600);
-  OpenLog.begin(9600);
 
   Wire.begin(I2C_ADDRESS);
+  Wire.onReceive(receiveEvent);
   Wire.onRequest(requestEvent);
 }
 
 void loop() {
-   pinMode(pingPin, OUTPUT);
-   digitalWrite(pingPin, LOW);
+   pinMode(US_TRIG, OUTPUT);
+   digitalWrite(US_TRIG, LOW);
    delayMicroseconds(2);
-   digitalWrite(pingPin, HIGH);
+   digitalWrite(US_TRIG, HIGH);
    delayMicroseconds(10);
-   digitalWrite(pingPin, LOW);
-   pinMode(echoPin, INPUT);
-   duration = pulseIn(echoPin, HIGH);
+   digitalWrite(US_TRIG, LOW);
+   pinMode(US_ECHO, INPUT);
+   duration = pulseIn(US_ECHO, HIGH);
    readingTime = millis();
    
    Serial.print("time ");
@@ -38,21 +35,25 @@ void loop() {
    Serial.print(" duration ");
    Serial.print(duration);
    Serial.println();
-
-   OpenLog.print(readingTime);
-   OpenLog.print(",");
-   OpenLog.print(duration);
-   OpenLog.println();
    
    delay(100);
 }
 
+void receiveEvent(int howMany) {
+  if (Wire.available() == 1) opcode = Wire.read();
+}
+
 void requestEvent() {
   // Read from the register variable to know what to send back
+  Serial.println(opcode == REGISTER_DURATION);
   if (opcode == REGISTER_DURATION) {
-    Wire.write((uint8_t *)&duration, sizeof(duration));
+    uint8_t * send = (uint8_t *) &duration;
+    Serial.println(*send);
+    int size = sizeof(duration);
+    Wire.write(send, size);
   } if (opcode == REGISTER_TIME) {
-    Wire.write((uint8_t *)&readingTime, sizeof(readingTime));
+    Serial.println(readingTime);
+    Wire.write((uint8_t *) &readingTime, sizeof(readingTime));
   } else {
     Wire.write(0);
   }
